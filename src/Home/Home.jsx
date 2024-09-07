2df529b3ec32d19a4a790a0f2c7840de44806b2d
Md Repon <repon7253@gmail.com>
2024-09-07 09:48:46 +0600

    k

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from './Banner'
 import HomeCategory from './HomeCategory'
 import CategoryShowCase from './CategoryShowCase'
+import Register from './Register'
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
         <Banner/>
         <HomeCategory/>
         <CategoryShowCase/>
+        <Register/>
       </div>
     </div>
   )
---
+++ src/Home/HomeSprade.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeSprade = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HomeSprade
